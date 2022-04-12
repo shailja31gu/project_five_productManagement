433e97edd6f6b05a8ed2607dfe9ef64e39d2391f
minakshiMandoli <mandoliminakshi@gmail.com>
2022-04-12 02:49:46 -0700

    update user profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,10 +5,11 @@ const aws = require("aws-sdk");
 
 const saltRounds = 10;
 const { isValid, isValidRequestBody, isValidObjectId } = require('../validator/validator');
+const { hash } = require('bcrypt');
 
 
 const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,15}$/
+//const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,15}$/
 
 
 //---------AWS S3..............................................................
@@ -70,7 +71,7 @@ const registerUser = async function (req, res) {
         if (duplicatePhone) {
             return res.status(400).send({ status: false, message: `Phone Number Already Present` });
         }
-        if (!isValid(password.trim())) { return res.status(400).send({ status: "false", message: "Please enter a valid password" }) }
+        if (!isValid(password)) { return res.status(400).send({ status: "false", message: "Please enter a valid password" }) }
         if (!(password.length >= 8 && password.length <= 15)) {
             return res.status(400).send({ status: false, message: "Password should be Valid min 8 and max 15 " });
         }
@@ -124,7 +125,7 @@ const registerUser = async function (req, res) {
         if (files && files.length > 0) {
             profileImage = await uploadFile(files[0])
         }
-        else {  return res.status(400).send({ message: "No file found" }) }
+        else { return res.status(400).send({ message: "No file found" }) }
         const hash = bcrypt.hashSync(password, saltRounds)
         const updatedData = {
             "fname": fname,
@@ -136,7 +137,7 @@ const registerUser = async function (req, res) {
             "profileImage": profileImage,
         }
         let user = await userModel.create(updatedData)
-         return res.status(201).send({ status: true, message: "user registered succesfully", data: user })
+        return res.status(201).send({ status: true, message: "user registered succesfully", data: user })
     }
     catch (err) {
         return res.status(500).send({ status: "false", message: err.message })
@@ -159,40 +160,16 @@ const userLogin = async (req, res) => {
         if (!emailRegex.test(email)) {
             return res.status(400).send({ status: false, message: "Email should be valid email" });
         }
-
         if (!isValid(password)) {
             return res.status(400).send({ status: false, message: "Password is required" });
         }
-
-        // if (!passwordRegex.test(password)) {
-        //     return res.status(400).send({ status: false, message: "Password should be valid Password" });
-        // }
         const user = await userModel.findOne({ email });
         if (!user) {
             return res.status(404).send({ status: false, message: "User doesn't exists. Please register first" })
         }
-
         if (!user.email) {
             return res.status(404).send({ status: false, message: "Email and Password not matched" }); // wrong email
         }
-
-        // bcrypt.compare(password, user.password, function (err, res) {
-
-        //     if (res) {
-        //         const token = jwt.sign({
-        //             userId: user._id
-        //         }, 'Group03', { expiresIn: 60 * 60 });
-        //         res.setHeader("Bearer token", token);
-
-        //         return res.status(200).send({ 'status': true, message: "User login successfull", data: { token, userId: user._id } });
-
-        //     } else {
-        //         return res.status(401).send({ status: false, message: err.message });
-        //     }
-        //     // if res == true, password matched
-        //     // else wrong password
-        // });
-
         const isMatched = await bcrypt.compare(password, user.password);
         if (!isMatched) {
             return res.status(401).send({ status: false, message: "Password not matched" });
@@ -204,18 +181,12 @@ const userLogin = async (req, res) => {
 
         return res.status(200).send({ 'status': true, message: "User login successfull", data: { userId: user._id, token } });
 
-
-        // if (!user.password) {
-        //     return res.status(404).send({ status: false, message: "Email and Password not matched" }); // wrong password
-        // }
-
-
     } catch (error) {
         return res.status(500).send({ status: false, message: error.message });
     }
 
 }
-
+/////.........get user........................................................................
 const getUser = async (req, res) => {
     try {
         const id = req.params.userId
@@ -233,19 +204,138 @@ const getUser = async (req, res) => {
             return res.status(401).send({ status: false, message: "You are not authorized" })
         }
         return res.status(200).send({ status: true, message: 'User profile details', data: userDetails })
-
     }
     catch (err) {
         return res.status(500).send({ status: false, message: err.message })
     }
+}
+////////////////////...........update user..........................................................
 
+const updateUser = async function (req, res) {
+    try {
+        const userId = req.params.userId
+        if (!isValidObjectId(userId)) {
+            return res.status(400).send({ status: false, message: ' invalid user id' })
+        }
+        const userFound = await userModel.findOne({ _id: userId })
+        if (!userFound) {
+            return res.status(400).send({ status: false, message: 'no user exist with such user id' })
+        }
+        if (userFound._id != req.user) {
+            return res.status(401).send({ status: false, message: 'Unauthorized user to upade the profile' })
+        }
+        const userData = req.body
+        const files = req.files
+
+        let { fname, lname, email, phone, password, address, profileImage } = userData
+        if (fname) {
+            if (!isValid(fname)) {
+                { return res.status(400).send({ status: "false", message: "Please enter first name" }) }
+            }
+        }
+        if (lname) {
+            if (!isValid(lname)) {
+                { return res.status(400).send({ status: "false", message: "Please enter last name" }) }
+            }
+        }
+        if (email) {
+            if (!isValid(email)) {
+                { return res.status(400).send({ status: "false", message: "Please enter first name" }) }
+            }
+
+            if (!/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(email)) {
+                return res.status(400).send({ status: false, message: `Email should be a valid email address` });
+            }
+            let duplicateEmail = await userModel.findOne({ email: email })
+            if (duplicateEmail) {
+                return res.status(400).send({ status: false, message: `Email Already Exist` });
+            }
+        }
+        if (phone) {
+            if (!isValid(phone)) {
+                return res.status(400).send({ status: false, message: "Invalid request parameter, please provide Phone" });
+            }
+            if (!/^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/.test(phone)) {
+                return res.status(400).send({ status: false, message: `Mobile should be a valid number` });
+            }
+            let duplicatePhone = await userModel.findOne({ phone: phone })
+            if (duplicatePhone) {
+                return res.status(400).send({ status: false, message: `Phone Number Already Present` });
+            }
+        }
+        if (password) {
+            if (!isValid(password)) { return res.status(400).send({ status: "false", message: "Please enter a valid password" }) }
+            if (!(password.length >= 8 && password.length <= 15)) {
+                return res.status(400).send({ status: false, message: "Password should be Valid min 8 and max 15 " });
+            }
+            var hash = bcrypt.hashSync(password, saltRounds)
+        }
+        if (address) {
+            address = JSON.parse(address)
+
+            if (address.shipping) {
+                if (address.shipping.street) {
+                    if (!isValid(address.shipping.street)) {
+                        return res.status(400).send({ status: false, message: 'Shipping Street Required' });
+                    }
+                }
+
+                if (address.shipping.city) {
+                    if (!isValid(address.shipping.city)) {
+                        return res.status(400).send({ status: false, message: 'Shipping city is Required' });
+                    }
+                }
+                if (address.shipping.pincode) {
+                    if (!isValid(address.shipping.pincode)) {
+                        return res.status(400).send({ status: false, message: 'Shipping pincode Required' });
+                    }
+                }
+            }
+            if (address.billing) {
+                if (address.billing.street) {
+                    if (!isValid(address.billing.street)) {
+                        return res.status(400).send({ status: false, message: 'billing Street Required' })
+                    }
+                }
+                if (address.billing.city) {
+                    if (!isValid(address.billing.city)) {
+                        return res.status(400).send({ status: false, message: 'billing city is Required' });
+                    }
+                }
+                if (address.billing.pincode) {
+                    if (!isValid(address.billing.pincode)) {
+                        return res.status(400).send({ status: false, message: 'billing pincode Required' });
+                    }
+                }
+            }
+        }
+
+        if (files) {
+            if (files.length > 0) {
+                profileImage = await uploadFile(files[0])
+            }
+        }
+            const updatedUser = await userModel.findOneAndUpdate({ _id: userId }, {
+                $set: {
+                    fname: fname,
+                    lname: lname,
+                    email: email,
+                    phone: phone,
+                    password: hash,
+                    address: address,
+                    profileImage: profileImage,
+                }
+            }, { new: true })
+            return res.status(200).send({ status: true, message: "user updated succesfully", data: updatedUser })
+        }
+    
+
+    catch (err) {
+        return res.status(500).send({ status: false, message: err.message })
+    }
 }
 
 
-module.exports = { userLogin, registerUser, getUser }
 
+module.exports = { userLogin, registerUser, getUser, updateUser }
 
-// bcrypt.compare(myPlaintextPassword, hash, function(err, res) {
-    // if res == true, password matched
-    // else wrong password
- // });
